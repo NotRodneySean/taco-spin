451de618263d8776a3d4d5a9078e22df57a6add2
Rodney Sean <backlightdesigns@gmail.com>
2021-05-25 11:23:38 -0600

    Update

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,8 +1,10 @@
 let User = {};
+let total_wins = 0;
 
 function displayPastWinners(totals) {
-    totals = User.totals_list;
     let content = $("#winningHistory tbody");
+    totals = User.totals_list;
+
     for (let i = 0; i < totals.length; i++) {
         content.append('<tr>' + '<td>' + totals[i].name + '</td>' + '<td>' + totals[i].guess + '</td>' + '<td>' + totals[i].spin_count + '</td>' + '<td>' + totals[i].date_won + '</td>' + '</tr>');
     }
@@ -11,7 +13,6 @@ function displayPastWinners(totals) {
 $.ajax({
     url: '/history',
     complete: function (data) {
-        console.log(data);
         User = JSON.parse(data.responseText);
         displayPastWinners(User.totals);
     }
